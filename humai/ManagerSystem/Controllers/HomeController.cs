d22f3366398feeadda353c8fbbbae2045b73b33c
huangbo <530580065@qq.com>
2016-12-07 11:36:52 +0800

    导入项目

---
+++ humai/ManagerSystem/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManagerSystem.Models;
+
+namespace ManagerSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private BookContext dbBook = new BookContext(); 
+        public ActionResult Index()
+        {
+            var books = from b in dbBook.Books 
+                        where b.Author.Equals("Tom") 
+                        select b;
+            return View(books.ToList());
+        }
+
+        public ActionResult Welcome() {
+            return View();
+        }
+
+    }
+}
---
+++ humai/ManagerSystem/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagerSystem.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+        public int Author { get; set; }
+        public string Date { get; set; }
+    }
+}
